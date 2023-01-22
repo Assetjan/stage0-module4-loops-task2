1d666557935ebb4f6f93ff943abf5eba04e79a10
Assetjan <asetziyabek@gmail.com>
2023-01-22 21:27:06 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 2;
+        boolean a;
+        while (i <= printToInclusive) {
+            a = true;
+            int j = 2;
+            while (j<i){
+                if (i%j == 0){
+                    a = false;
+                }
+                j++;
+            }
+            if(a) System.out.println(i);
+            i++;
+        }
     }
 }
