9544c0ab5ca62e425d1c73ee1bbd86504d0f3dab
Assetjan <asetziyabek@gmail.com>
2023-01-22 20:48:08 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
+        int sum = 0;
+        while(sum <= printTillInclusive) {
+            System.out.println(sum);
+            sum += 2;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int sum = 1;
+        int count = 1;
+        int count2 = 0;
+        while (count2 <= printToInclusive){
+            System.out.println(sum);
+            sum = sum * count;
+            count++;
+            count2++;
+        }
     }
 }
