11933c9a1ce72cb1c016b3c28476a96872c33557
Assetjan <asetziyabek@gmail.com>
2023-01-22 21:30:45 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.loops.task2;
 public class PowerOfTwo {
     public void printPower(int power) {
         int count = 0;
-        if(power > 0){
+        if(power >= 0){
             while (count <= power){
-                System.out.println(Math.pow(2,count));
+                System.out.println((int)Math.pow(2,count));
                 count++;
             }
         }else{
