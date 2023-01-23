1526e20fd0e830964bdb48e562b80b039a0925e9
Assetjan <asetziyabek@gmail.com>
2023-01-23 09:08:17 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -4,7 +4,8 @@ public class WordsBuilder {
     public void buildPhrase(char... chars) {
         int count = 0;
         while(count < chars.length ) {
-            System.out.print(chars);
+            System.out.print(chars[count]);
+            count++;
         }
     }
 }
